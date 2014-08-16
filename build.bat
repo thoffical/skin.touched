0e390a398490c41a4969cad597bee32dc8f7e872
Martijn Kaijser <machine.sanctum@gmail.com>
2014-08-16 21:22:31 +0200

    update build.bat to match packaging folder rework

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 ECHO ----------------------------------------
 echo Creating Touched Build Folder
-IF Exist ..\..\project\Win32BuildSetup\BUILD_WIN32\Xbmc\addons\skin.touched rmdir ..\..\project\Win32BuildSetup\BUILD_WIN32\Xbmc\addons\skin.touched /S /Q
-md ..\..\project\Win32BuildSetup\BUILD_WIN32\Xbmc\addons\skin.touched\media\
+IF Exist ..\..\project\Win32BuildSetup\BUILD_WIN32\addons\skin.touched rmdir ..\..\project\Win32BuildSetup\BUILD_WIN32\addons\skin.touched /S /Q
+md ..\..\project\Win32BuildSetup\BUILD_WIN32\addons\skin.touched\media\
 
 Echo build.bat>>exclude.txt
 Echo .git>>exclude.txt
@@ -11,11 +11,11 @@ Echo exclude.txt>>exclude.txt
 
 ECHO ----------------------------------------
 ECHO Creating XBT File...
-START /B /WAIT ..\..\Tools\TexturePacker\TexturePacker -dupecheck -input media -output ..\..\project\Win32BuildSetup\BUILD_WIN32\Xbmc\addons\skin.touched\media\Textures.xbt
+START /B /WAIT ..\..\Tools\TexturePacker\TexturePacker -dupecheck -input media -output ..\..\project\Win32BuildSetup\BUILD_WIN32\addons\skin.touched\media\Textures.xbt
 
 ECHO ----------------------------------------
 ECHO XBT Texture Files Created...
 ECHO Building Skin Directory...
-xcopy "..\skin.touched" "..\..\project\Win32BuildSetup\BUILD_WIN32\Xbmc\addons\skin.touched" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "..\skin.touched" "..\..\project\Win32BuildSetup\BUILD_WIN32\addons\skin.touched" /E /Q /I /Y /EXCLUDE:exclude.txt
 
 del exclude.txt
